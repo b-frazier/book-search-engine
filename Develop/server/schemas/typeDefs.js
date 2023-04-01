61b307327b6619e281be1de50bee4c35de2a1c05
bfrazier <bthehippie2@gmail.com>
2023-04-01 19:18:10 -0400

    define mutation

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -12,5 +12,12 @@ const typeDefs = gql`
   type Query {
     me: [User]
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: InputBook!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 module.exports = typeDefs;
